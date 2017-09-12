93d7fb97bff58fd61892a90f4f8c2edf7c7b1259
Ori Hoch <ori@uumpa.com>
2017-09-12 22:50:42 +0300

    add redis k8s deployment (needed by back)

--- bin/k8s_apply.sh
+++ bin/k8s_apply.sh
@@ -2,5 +2,5 @@
 
 "${BEIT_HATFUTSOT_DEVOPS_DIR:-`pwd`}/bin/k8s_connect.sh"
 
-kubectl apply -f k8s/back.yaml -f k8s/front.yaml -f k8s/nginx.yaml
+kubectl apply -f k8s/back.yaml -f k8s/front.yaml -f k8s/nginx.yaml -f k8s/redis.yaml
 
