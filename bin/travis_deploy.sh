073cabea6f5a41bc62e080bf6df2f4733c846fa0
Ori Hoch <ori@uumpa.com>
2017-09-13 12:42:59 +0300

    travis

--- bin/travis_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-bin/k8s_apply.sh
--- bin/travis_script.sh
+++ bin/travis_script.sh
@@ -18,3 +18,6 @@ gcloud --quiet components update kubectl
 echo $GCLOUD_KEY | base64 --decode > gcloud.json
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.json
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
+
+# deploy
+bin/k8s_apply.sh
